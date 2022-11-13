c1955bca6d1bbc291966625e1caa2d68761faa77
minsu.rob <minsu.rob@gmail.com>
2022-11-14 00:58:01 +0900

    feat : add router package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 import firebase from "./firebase";
-console.log(firebase);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
