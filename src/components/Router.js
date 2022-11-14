09f1ac28cd19bb2d327081cdd3c610a09c10d9c2
minsu.rob <minsu.rob@gmail.com>
2022-11-14 15:46:32 +0900

    feat : add the profile nav, log out 구현

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,20 +1,29 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch /*, Redirect*/ } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile"
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
-          <Route exact path="/">
-            <Home />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
         ) : (
           <Route exact path="/">
             <Auth />
           </Route>
         )}
+        {/* <Redirect from="*" to="/"/> */}
       </Switch>
     </Router>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,16 @@
-const Profile = () => <span>Profile</span>;
+import { authService  } from "fbase";
+import { useHistory } from "react-router-dom";
+const Profile = () => {
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    };
+    const history = useHistory();
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    )
+}
 
 export default Profile;
