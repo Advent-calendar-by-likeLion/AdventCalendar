6e9179f95b14231654fc1671fb71b9b2735aff74
minsu.rob <minsu.rob@gmail.com>
2022-11-15 23:13:27 +0900

    feat : add the preview image on post

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import Nweet from "../components/Nweet";
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState("");
   
     useEffect(() => {
       dbService.collection("nweets").onSnapshot((snapshot) => {
@@ -34,6 +35,21 @@ const Home = ({ userObj }) => {
       } = event;
       setNweet(value);
     };
+
+    const onFileChange = (event) => {
+      const {
+        target: { files },
+      } = event;
+      const theFile = files[0];
+      const reader = new FileReader();
+      reader.onloadend = (finishedEvent) => {
+        const {
+          currentTarget: { result },
+        } = finishedEvent;
+        setAttachment(result);
+      }
+      reader.readAsDataURL(theFile);
+    }
   
     return (
       <>
@@ -45,7 +61,9 @@ const Home = ({ userObj }) => {
             placeholder="What's on your mind?"
             maxLength={120}
           />
+          <input type="file" accept="image/*" onChange={onFileChange}/>
           <input type="submit" value="Nweet" />
+          {attachment && <img src={attachment} width="50px" height="50px" />}
         </form>
         <div>
             {
