dd3123ba1f2804b1376e9ff5a76398901e2193c7
minsu.rob <minsu.rob@gmail.com>
2022-11-17 23:40:01 +0900

    fix : 함수형 컴포넌트 구조 변경

--- src/routes/Home2.js
+++ src/routes/Home2.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 import styled from "styled-components";
 
-function Home2() {
+const Home2 = ({ userObj }) => {
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ,22 , 23, 24, 25];
   return (
     <div style={{
