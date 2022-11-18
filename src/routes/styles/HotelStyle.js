ae38044a4e6f16314e93051ae88ebddc892f9b96
seocylucky <seocylucky@github.com>
2022-11-18 11:26:22 +0900

    Design: Hotel & Door Matching

---
+++ src/routes/styles/HotelStyle.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const HotelContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Hotelbg = styled.img`
+    position: fixed;
+    width: 319.37px;
+    height: 466.9px;
+
+    margin: 0 auto;
+    margin-top: 242px;
+`
+export const GridBox = styled.div `
+    position: relative;
+    display: grid;
+    width: 207.69px;
+    height: 358.8px;
+
+    margin-top: 275px;
+    justify-items: center;
+`
