97acbc438cb9a48f4248317743a5bb84dd99510b
minsu.rob <minsu.rob@gmail.com>
2022-11-26 00:56:56 +0900

    fix : unused css 삭제

--- src/routes/HotelColor.js
+++ src/routes/HotelColor.js
@@ -114,55 +114,6 @@ const Container = styled.div`
     align-items: center;
     text-align: center;
 `
-const NicknameInput = styled.form`
-    display: flex;
-    flex-direction: row;
-    justify-items: center;
-    justify-content: center;
-    align-items: center;
-
-    margin-top: 92.84px;
-    margin-bottom: 63px;
-`
-const InputStylenick = styled.input`
-    width: 115px;
-    height: 35px;
-    background-color: #FCF4E9;
-    color:  #000000;
-
-    border-color: transparent;
-    border-bottom-style: solid;
-    border-bottom-width: 1px;
-    border-bottom-color: #BAB8B5;
-    padding-left: 10px;
-    text-align: center;
-
-    font-style: normal;
-    font-weight: 400;
-    font-size: 18px;
-    line-height: 25px;
-
-    ::placeholder {
-        color: #BAB8B5;
-        font-style: normal;
-        font-weight: 400;
-        font-size: 18px;
-        line-height: 33px;
-        text-align: center; 
-    }
-
-    :focus {
-        outline: none;
-    }
-`
-const HotelSubCon = styled.div`
-    margin: 0 auto;
-`
-const HotelImg = styled.img`
-    width: 210px;
-    height: 315.16px;
-    margin-top: 27px;
-`
 const RoofLayout = styled.div`
     display: flex;
     flex-direction: column;
