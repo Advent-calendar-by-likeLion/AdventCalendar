8d9ce3c9c184f5396369b3f39060dd902ceb5446
minsu.rob <minsu.rob@gmail.com>
2022-11-26 02:26:03 +0900

    refac : radio로 인식이 안되던 현상 수정
    name으로 radio로 구분이 되는것으로 보임

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,7 +1,7 @@
-const ColorPicker = ({changeRoof, color, name}) => {
+const ColorPicker = ({changeRoof, color}) => {
     return (
         <div>
-            <input id={color} type="radio" name={name} value={color} onChange={changeRoof}/>
+            <input id={color} type="radio" name="color" onChange={changeRoof}/>
             <label for={color}>
                 <span style={{background: color}}>
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon"/>
