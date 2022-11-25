1e40221e0e93712d499b138a78157636f436287d
minsu.rob <minsu.rob@gmail.com>
2022-11-26 03:04:59 +0900

    fix : name이 같으면 라디오 버튼이 동일한 사항으로 체크 되던 현상 수정

--- src/components/ColorPickerBody.js
+++ src/components/ColorPickerBody.js
@@ -1,7 +1,7 @@
 const ColorPickerBody = ({changeBody, color, type}) => {
     return (
         <div>
-            <input id={color + type} type="radio" name="color" value={color} onChange={changeBody}/>
+            <input id={color + type} type="radio" name={type} value={color} onChange={changeBody}/>
             <label for={color + type}>
                 <span style={{background: color}}>
                     <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/242518/check-icn.svg" alt="Checked Icon"/>
