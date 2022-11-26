4011b8133bc1cb4f568031eecc74ede3263febbb
seocylucky <seocylucky@github.com>
2022-11-27 02:15:25 +0900

    fix: start 화면 눈 덮인 호텔로 전환

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Hotel from '../assets/Hotel.svg';
+import Hotel from '../assets/SnowHotel.svg';
 import LoginBar from '../assets/LoginBar.svg';
 import SignupBar from '../assets/SignupBar.svg';
 import Title from '../assets/Welcome.svg';
