d401c4ba4c1aa5f3e7a24e375c33fb37b6ae4549
dudtlstm <annpys1024@naver.com>
2022-11-26 18:42:21 +0900

    feat: WriteSuccess button 로그인 했을 때는 다르게 뜨게 설정

--- src/routes/WriteSuccess.js
+++ src/routes/WriteSuccess.js
@@ -14,49 +14,53 @@ const WriteSuccess= ({userObj}) => {
     const onclickSignupBar = () => {
         history.push("/signup");
     }
-  return (
+    const onclickMyHotelBar = () => {
+        history.push("/hotel/" + id);
+    }
+    const onclickWriteLetterBar = () => {
+        history.push("/"); // 경로 설정 추후에 다시 할 예정
+    }
+    return (
 
     id === 0 ?  
     <>
-          <Container>
-          <LetterStyle src={Letter} />
-          <WriteSuccessTitle>편지를 보냈어요!</WriteSuccessTitle>
-          <WriteSubTitle>아직 내 호텔이 없나요?</WriteSubTitle>
-          <WriteSubTitle>내 호텔을 만들면</WriteSubTitle>
-          <WriteSubTitle>친구에게 편지를 받을 수 있어요.</WriteSubTitle>
-          <ButtonLayout>
-            {
-               
-            }
-            <RedButton onClick={onclickSignupBar}>내 호텔 만들기</RedButton>
-            <WhiteButton onClick={onclickLoginBar}>로그인</WhiteButton>
-          </ButtonLayout>
-      </Container>
+            <Container>
+            <LetterStyle src={Letter} />
+                <WriteSuccessTitle>편지를 보냈어요!</WriteSuccessTitle>
+                <WriteSubTitle>아직 내 호텔이 없나요?</WriteSubTitle>
+                <WriteSubTitle>내 호텔을 만들면</WriteSubTitle>
+                <WriteSubTitle>친구에게 편지를 받을 수 있어요.</WriteSubTitle>
+                <ButtonLayout>
+                    {
+            
+                    }
+                    <RedButton onClick={onclickSignupBar}>내 호텔 만들기</RedButton>
+                    <WhiteButton onClick={onclickLoginBar}>로그인</WhiteButton>
+                </ButtonLayout>
+            </Container>
     </> :
     <>
-          <Container>
-          <LetterStyle src={Letter} />
-          <WriteSuccessTitle>편지를 보냈어요!</WriteSuccessTitle>
-          <WriteSubTitle>아직 내 호텔이 없나요?</WriteSubTitle>
-          <WriteSubTitle>내 호텔을 만들면</WriteSubTitle>
-          <WriteSubTitle>친구에게 편지를 받을 수 있어요.</WriteSubTitle>
-          <ButtonLayout>
-            {
-               
-            }
-          </ButtonLayout>
-      </Container>
-    </>
-    
+            <Container>
+            <LetterStyle src={Letter} />
+                <WriteSuccessTitle>편지를 성공적으로 보냈어요!</WriteSuccessTitle>
+                <WriteSubTitle>이젠 어떤 걸 해볼까요?</WriteSubTitle>
+                <ButtonLayout>
+                    {
 
-  )
+                    }
+                    <RedButton onClick={onclickWriteLetterBar}>편지 더 쓰러 가기</RedButton>
+                    <WhiteButton onClick={onclickMyHotelBar}>내 호텔 보러 가기</WhiteButton>
+                </ButtonLayout>
+            </Container>
+    </>
+    )
 }
 
 export default WriteSuccess
 
 const WriteSuccessTitle = styled.div`
     text-align: center;
-    width: 141px;
+    width: 210px;
     height: 29px;
 `
 const ButtonLayout = styled.div`
