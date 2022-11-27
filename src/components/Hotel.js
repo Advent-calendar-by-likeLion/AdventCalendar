63a92e54ab82132049db5223c7ce1a53b22992a9
minsu.rob <minsu.rob@gmail.com>
2022-11-28 00:06:33 +0900

    fix : refactor the code indent

--- src/components/Hotel.js
+++ src/components/Hotel.js
@@ -1,4 +1,4 @@
-import { GridBox, Hotelbg, HotelContainer } from '../routes/styles/HotelStyle';
+import {GridBox, Hotelbg, HotelContainer} from '../routes/styles/HotelStyle';
 import Hotel2 from '../assets/Hotel2.svg';
 import styled from 'styled-components';
 import OpenWindow from './Window/OpenWindow';
@@ -14,39 +14,37 @@ const Hotel = () => {
     const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 ,22 , 23, 24, 25];
     return (
         <HotelContainer>
-          <Hotelbg src={Hotel2} />
-          <GridBox>
-            {items.map((item, key) => (
-                <div
-                  key={key}
-                  className={`div${item}`}
-                >
-                  {
-                  item == 1 ?
-                  <Btn onClick={openModalPost}>
-                    <OpenWindow/>
-                  </Btn>
-                 : 
-                  
-                  item == 24 ? 
-                    <Door/>
-                  :
-                  item == 25 ?
-                    <TopWindow/>
-                  :
-                    <Window/>
-                  }
-                </div>
-                
-              ))}
-          </GridBox>
+            <Hotelbg src={Hotel2}/>
+            <GridBox> {
+                items.map((item, key) => (
+                    <div key={key} className={`div${item}`}>
+                        {
+                        item == 1 ? 
+                        <Btn onClick={openModalPost}>
+                            <OpenWindow/>
+                        </Btn> 
+                        : 
+                        item == 24 ? 
+                          <Door/>
+                        : 
+                        item == 25 ? 
+                          <TopWindow/>
+                        : 
+                          <Btn onClick={openModalPost}>
+                            <Window/>
+                          </Btn>
+                        } 
+                    </div>
+
+                ))
+            } </GridBox>
         </HotelContainer>
     );
 }
 
 export default Hotel
 
-export const Btn = styled.button`
+export const Btn = styled.button `
   background-color: transparent !important;
   background-image: none !important;
   border-color: transparent;
