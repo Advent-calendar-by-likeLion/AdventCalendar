a9e07486711b0040e54b0aa1847d13347c8a676e
dudtlstm <annpys1024@naver.com>
2022-12-01 01:28:46 +0900

    feat: 내 호텔에서도 서비스 소개 모달 추가

--- src/routes/Home2.js
+++ src/routes/Home2.js
@@ -19,6 +19,16 @@ import MessageModal from "../components/Modal/MessageModal";
 // 쿠키 관련 모음
 import BellBoyCookie from '../assets/cookie/BellBoyCookie.svg';
 
+// Landing Page 관련
+import { LandingRedButton, LandingTitle1, LandingTitle2, LandingTitle3, LandingTitle4, LandingContent1, LandingContent2, LandingContent3, LandingContent4 } from './styles/style';
+import LandingModal from '../components/Modal/LandingModal';
+import { LandingPageModalInner } from "../components/Modal/styles";
+import LandingPage from '../assets/LandingPage/WelcometoGingerHotel.svg';
+import LandingModalButton from '../assets/LandingPage/LandingModalButton.svg';
+import LandingImage from '../assets/LandingPage/Landing_Image.svg';
+import LandingEmail from '../assets/LandingPage/LandingEmailImage.svg';
+import LandingInsta from '../assets/LandingPage/LandingInstaImage.svg';
+
 const Home2 = ({ userObj }) => {
   const history = useHistory();
   const {id} = useParams(); // hetelOwnerId
@@ -136,6 +146,17 @@ const Home2 = ({ userObj }) => {
     setGingerModalOpen((prev) => !prev);
   }
 
+  const [isLandingModalOpen, setLandingModalOpen] = useState(false);
+
+  const onClickOpenLandingModal = () => {
+      setLandingModalOpen(true);
+  }
+  
+  const onClickCloseLandingModal = () => {
+      setLandingModalOpen((prev) => !prev);
+  }
+
+
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
@@ -164,6 +185,7 @@ const Home2 = ({ userObj }) => {
           <Hotel/>
           { 
           id === (userObj ? userObj.uid : 0) ?  
+          
           <>
             <RedButton disabled={!isFull} onClick={onClickOpenModal}>오늘의 편지</RedButton>
             {!isFull ? <><br/><HotelGuide>* 오늘의 편지를 채워야 열람할 수 있어요! *</HotelGuide></>:<></>}
@@ -191,7 +213,10 @@ const Home2 = ({ userObj }) => {
             
             <br/>
           </>
-          }          
+          }   
+          <LandingButtonHotel1>
+            <img src={LandingModalButton} onClick={onClickOpenLandingModal}/>
+          </LandingButtonHotel1>       
           {isModalOpen && <Modal closeModal={onClickCloseModal}>
                             <h1>도착한 편지</h1>
                             <CardLayout>
@@ -220,6 +245,48 @@ const Home2 = ({ userObj }) => {
                               <RedRoundButton>편지 읽기</RedRoundButton>
                             </GingerCardLayout>
                           </GingerModal>}
+          {isLandingModalOpen && <LandingModal closeModal={onClickCloseLandingModal}>
+                            <LandingPageModalInner>
+                                <LandingRedButton src={LandingPage} />
+                                <LandingTitle1>⛄어드벤트 캘린더란?</LandingTitle1>
+                                <LandingContent1>
+                                    어드벤트 캘린더는 12월 1일부터 25일까지, 
+                                    <br/>크리스마스를 기다리며 하나씩 선물을 열어보는 달력을 말해요! 한국에서는 아직 대중화되지 
+                                    <br/>않았지만, 외국에서는 크리스마스와 연말 시즌에 많이 사용한답니다.</LandingContent1>
+                                <br/>
+                                <LandingTitle2>⛄진저호텔 이용방법</LandingTitle2>
+                                <LandingContent2>
+                                    🎄 내 호텔을 만들고 SNS에 링크를 공유해요.
+                                    <br/>
+                                    🎄 친구들에게 편지를 받으면 창문을 열 수 있어요.
+                                    <br/>
+                                    🎄 창문 안에는 친구들이 보내준 메세지가 들어 있어요.
+                                    <br/>
+                                    🎄 하루에 하나만 오픈 가능해요!
+                                    <br/>
+                                    🎄 정해진 편지 갯수를 채워야 창문을 열 수 있어요!
+                                </LandingContent2>
+                                <br/>
+                                <LandingTitle3>⛄진저맨 카드를 모두 모아 보세요!</LandingTitle3>
+                                <LandingContent3>진저호텔에 사는 25종의 진저맨을 모두 모아 보세요!</LandingContent3>
+                                <LandingGingerImage>
+                                    <img src={LandingImage}/>
+                                </LandingGingerImage>
+                                <br/>
+                                <LandingTitle4>⛄웰컴 투 진저호텔</LandingTitle4>
+                                <LandingContent4>
+                                    🎅 웰컴 투 진저호텔은 광운대, 동국대, 숭실대,
+                                    <br/>중앙대, 한서대 학생 5명이 함께 만든 크리스마스 시즌 서비스입니다.
+                                    <br/>🎅 웰컴 투 진저호텔은 수익을 창출하지 않으며, 비영리 서비스입니다.
+                                </LandingContent4>
+                                <LandingInstaImage>
+                                    <img src={LandingInsta}/>
+                                </LandingInstaImage>
+                                <LandingEmailImage>
+                                    <img src={LandingEmail}/>
+                                </LandingEmailImage>
+                            </LandingPageModalInner>
+            </LandingModal>}
       </HotelContainer>
     </>
   );
@@ -315,4 +382,33 @@ const HotelGuide = styled.div`
     text-align: center; 
     font-weight: 500;
     font-size: 12px;
+`
+
+const LandingGingerImage = styled.div`
+    position: relative;
+    height: 116px;
+    width: 220px;
+    left: 0px;
+    top: 143px;
+`
+const LandingInstaImage = styled.div`
+    position: relative;
+    height: 10px;
+    width: 10px;
+    left: 40px;
+    top: 185px;
+`
+const LandingEmailImage = styled.div`
+    position: relative;
+    height: 10px;
+    width: 10px;
+    left: 40px;
+    top: 190px;
+`
+const LandingButtonHotel1 = styled.div`
+    position: relative;
+    width: 35px;
+    height: 35px;
+    left: 160px;
+    top: -993px;
 `
