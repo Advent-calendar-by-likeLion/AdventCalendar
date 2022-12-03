e6787daf0026365c52aba3cdd71dabb145aa5039
khsrla9806 <kimhunsope@naver.com>
2022-12-03 21:08:17 +0900

    fix: 어드민 페이지 접근 권한 설정

--- src/routes/AdminConfig.js
+++ src/routes/AdminConfig.js
@@ -13,8 +13,13 @@ const AdminConfig = ({userObj}) => {
     const [windowCount, setWindowCount] = useState(2);
     const [windowInfoIndex, setWindowInfoIndex] = useState(0);
     const history = useHistory();
+    const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
+
+        if (userObj.uid == "sksKvNWFPBe5ZmJtNCdbGUh5jYk1") {
+            setIsAdmin(true);
+        }
 
         /*dbService.collection("AdminConfig").doc("AdminConfig").get()
         .then((doc) => {
@@ -62,10 +67,6 @@ const AdminConfig = ({userObj}) => {
     const onSubmitWindowClose = async (event) => {
         event.preventDefault();
 
-        // dbService.collection("hotelOwner").doc(id).onSnapshot((doc) => {
-        //     setWindowInfo(doc.data().windowInfo);
-        // });
-
         await dbService.collection("hotelOwner").doc(id).update({
             [`windowInfo.${windowInfoIndex}`] : false,
            //  windowCount : i,
@@ -95,70 +96,93 @@ const AdminConfig = ({userObj}) => {
         alert(`오늘의 편지 갯수 ${windowCount}개 로 변경`);
         //history.push("/hotel/" + userObj.uid);
     }
+    const toHome = () => {
+        history.push("/");
+    }
     
 
   return (
-      <>
-        <Container>
+        isAdmin ?
+        (
+            <>
+                <Container>
 
-            <div style={{marginTop: "141px", fontSize: '30px', fontWeight: "bold"}}>관리자페이지</div>
-            <br/>
-            
-            <br/>
-            <div style={{
-                height: "29px",
-                fontSize: "20px",  
-                fontWeight: "500px",
+                    <div style={{marginTop: "141px", fontSize: '30px', fontWeight: "bold"}}>관리자페이지</div>
+                    <br/>
+                    
+                    <br/>
+                    <div style={{
+                        height: "29px",
+                        fontSize: "20px",  
+                        fontWeight: "500px",
 
-            }}>창문 열기</div>
-            <NicknameInput>
-                <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowInfoIndex}/>
-            </NicknameInput>
-            <br/>
-            <form onSubmit={onSubmitWindowInfo}>
-                <RedButton>창문 열기</RedButton>
-            </form>
+                    }}>창문 열기</div>
+                    <NicknameInput>
+                        <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowInfoIndex}/>
+                    </NicknameInput>
+                    <br/>
+                    <form onSubmit={onSubmitWindowInfo}>
+                        <RedButton>창문 열기</RedButton>
+                    </form>
 
-            <br/>
-            <div style={{
-                height: "29px",
-                fontSize: "20px",  
-                fontWeight: "500px",
+                    <br/>
+                    <div style={{
+                        height: "29px",
+                        fontSize: "20px",  
+                        fontWeight: "500px",
 
-            }}>창문 닫기</div>
-            <NicknameInput>
-                <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowInfoIndex}/>
-            </NicknameInput>
-            <br/>
-            <form onSubmit={onSubmitWindowClose}>
-                <RedButton>창문 닫기</RedButton>
-            </form>
+                    }}>창문 닫기</div>
+                    <NicknameInput>
+                        <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowInfoIndex}/>
+                    </NicknameInput>
+                    <br/>
+                    <form onSubmit={onSubmitWindowClose}>
+                        <RedButton>창문 닫기</RedButton>
+                    </form>
 
-            <br/>
-            <br/>
-            <div style={{
-                height: "29px",
-                fontSize: "20px",  
-                fontWeight: "500px",
+                    <br/>
+                    <br/>
+                    <div style={{
+                        height: "29px",
+                        fontSize: "20px",  
+                        fontWeight: "500px",
 
-            }}>X일차 창문 컨트롤</div>
-            <HotelGuide>사용자가 오류로 인해, 날짜가 하루 지나도 다음 창문으로 카운팅 되지않을때 사용</HotelGuide>
-            <HotelGuide>사용법: url에 AdventCalendar#/adminConfig/(ID) 를 삽입</HotelGuide>
-            <HotelGuide>예시: 1일차에 10개를 쌓았으나, 2일차에도 10개에 계속 이어간다면, 숫자를 2를 주면 2번째 창문으로 넘어감.</HotelGuide>
-            <NicknameInput>
-                <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowCount}/>
-            </NicknameInput>
-            <br/>
-            <form onSubmit={onSubmitWindowCount}>
-                <RedButton>창문 일자 변경</RedButton>
-            </form>
+                    }}>X일차 창문 컨트롤</div>
+                    <HotelGuide>사용자가 오류로 인해, 날짜가 하루 지나도 다음 창문으로 카운팅 되지않을때 사용</HotelGuide>
+                    <HotelGuide>사용법: url에 AdventCalendar#/adminConfig/(ID) 를 삽입</HotelGuide>
+                    <HotelGuide>예시: 1일차에 10개를 쌓았으나, 2일차에도 10개에 계속 이어간다면, 숫자를 2를 주면 2번째 창문으로 넘어감.</HotelGuide>
+                    <NicknameInput>
+                        <InputStylenick type="text" placeholder='일자 입력' onChange={onChangeWindowCount}/>
+                    </NicknameInput>
+                    <br/>
+                    <form onSubmit={onSubmitWindowCount}>
+                        <RedButton>창문 일자 변경</RedButton>
+                    </form>
 
-            <br/>
-            <WhiteButton onClick={ () => {
-                history.push("/");
-            } } >시작화면</WhiteButton>
-        </Container>
-      </>
+                    <br/>
+                    <WhiteButton onClick={ () => {
+                        history.push("/");
+                    } } >시작화면</WhiteButton>
+                </Container>
+            </>
+        )
+        :
+        (
+            <>
+                <Container>
+                    <HotelGuide style={{marginTop:'250px', fontSize:"30px", fontFamily:"humanbeomseok"}}>
+                        혹시 해킹하시려는건 아니죠?
+                    </HotelGuide>
+                    <HotelGuide style={{marginTop:'30px', fontSize:"30px", fontFamily:"humanbeomseok"}}>
+                        그런거라면 저희 너무 슬픈데...
+                    </HotelGuide>
+                    <HotelGuide style={{marginTop:'30px', fontSize:"30px", fontFamily:"humanbeomseok"}}>
+                        우리 호텔을 만들어볼까요?
+                    </HotelGuide>
+                    <RedButton style={{marginTop:'70px'}} onClick={toHome}>호텔 만들러가기</RedButton>
+                </Container>
+            </>
+        )
   )
 }
 
