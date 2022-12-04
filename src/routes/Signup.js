4f11e7dba768a198553977abb234bc6bfc74b744
khsrla9806 <kimhunsope@naver.com>
2022-12-05 01:03:48 +0900

    fix: 회원가입 주의 문구 추가

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -53,6 +53,9 @@ const Signup = ({userObj}) => {
         {/* <TitleDiv>진저호텔에서 보내는 25일간의 휴일</TitleDiv> */}
         <form onSubmit={onSubmit}>
           <InputLayout2>
+              <SignupGuide>이메일과 비밀번호는 나중에 찾을 수 없습니다.</SignupGuide>
+              <SignupGuide>천천히 정확하게 입력해주세요.</SignupGuide>
+              <br/><br/><br/>
               <InputStyle placeholder='이메일' name="email" type="email" required value={email} onChange={onChange} />
               <InputStyle placeholder='비밀번호' name="password1" type="password" required value={password1} onChange={onChange} />
               <InputStyle placeholder='비밀번호 확인' name="password2" type="password" required value={password2} onChange={onChange}/>
@@ -84,4 +87,12 @@ const ButtonLayout = styled.div`
     margin-top: 62.02px;
     margin-bottom: 142px;
     align-items: center;
+`
+
+const SignupGuide = styled.div`
+    text-align: center; 
+    font-weight: 500;
+    font-size: 12px;
+    margin-bottom: 5px;
+    font-family: humanbeomseok;
 `
